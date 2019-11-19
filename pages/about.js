cad900e59804ff3f4c00e2a6d1310fffcd74436e
Joe Haddad <joe.haddad@zeit.co>
2019-11-18 20:05:45 -0500

    Snapshot of Styling your Application

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,19 @@
 export default function() {
   return (
-    <main>
-      <h1>About</h1>
-      <p>This is a cool about page.</p>
-    </main>
+    <>
+      <main>
+        <h1>About</h1>
+        <p>This is a cool about page.</p>
+      </main>
+      <style jsx>{`
+        h1 {
+          color: blue;
+        }
+
+        p {
+          font-size: 1.25rem;
+        }
+      `}</style>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
 export default function() {
-  return <p>Hello World</p>;
+  return (
+    <>
+      <p>Hello World</p>
+      <style global jsx>{`
+        body {
+          font-family: "SF Pro Text", "SF Pro Icons", "Helvetica Neue",
+            "Helvetica", "Arial", sans-serif;
+          padding: 20px 20px 60px;
+          max-width: 680px;
+          margin: 0 auto;
+          font-size: 16px;
+          line-height: 1.65;
+          word-break: break-word;
+          font-kerning: auto;
+          font-variant: normal;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          text-rendering: optimizeLegibility;
+          hyphens: auto;
+        }
+      `}</style>
+    </>
+  );
 }
