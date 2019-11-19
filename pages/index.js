7ca98cb5344f845726808f9906021c396dc26982
Joe Haddad <joe.haddad@zeit.co>
2019-11-18 23:27:35 -0500

    Snapshot Babel Configuration

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Link from "next/link";
 import Head from "next/head";
 
 function Home() {
+  let object = { a: null, b: { foo: "bar" } };
   return (
     <>
       <Head>
         <title>Demo - Home</title>
       </Head>
-      <p>Hello World</p>
+      <p>Hello World {object.b?.foo || "N/A"}</p>
       <Link href="/about">
         <a>About</a>
       </Link>
