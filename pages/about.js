a9f6291a9febf683e86bd0d0e9d7e001cc2f0e1f
Joe Haddad <joe.haddad@zeit.co>
2019-11-18 20:23:16 -0500

    Snapshot of `<NextLink>`

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function() {
   return (
     <>
       <main>
         <h1>About</h1>
         <p>This is a cool about page.</p>
+        <Link href="/">
+          <a>Go Home</a>
+        </Link>
       </main>
       <style jsx>{`
         h1 {
@@ -12,6 +17,18 @@ export default function() {
 
         p {
           font-size: 1.25rem;
+        }
+
+        a {
+          cursor: pointer;
+          color: #0076ff;
+          text-decoration: none;
+          transition: all 0.2s ease;
+          border-bottom: 1px solid white;
+        }
+
+        a:hover {
+          border-bottom: 1px solid #0076ff;
         }
       `}</style>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function() {
   return (
     <>
       <p>Hello World</p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
       <style global jsx>{`
         body {
           font-family: "SF Pro Text", "SF Pro Icons", "Helvetica Neue",
@@ -18,6 +23,19 @@ export default function() {
           -moz-osx-font-smoothing: grayscale;
           text-rendering: optimizeLegibility;
           hyphens: auto;
+        }
+      `}</style>
+      <style jsx>{`
+        a {
+          cursor: pointer;
+          color: #0076ff;
+          text-decoration: none;
+          transition: all 0.2s ease;
+          border-bottom: 1px solid white;
+        }
+
+        a:hover {
+          border-bottom: 1px solid #0076ff;
         }
       `}</style>
     </>
